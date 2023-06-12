b194eb21cd6865d1d0c9f3dfd2c4be7eaa5d8558
Aurelie Lescop <lescop.aurelie@live.fr>
2023-06-12 18:33:46 +0200

    connexion API au cluster MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
+const mongoose = require('mongoose');
+
 const express = require('express');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://Lily:uxBLzmcRsOqJqrsC@cluster0.mzvepik.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
